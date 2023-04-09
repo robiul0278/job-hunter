7e2030ef0bef26f8ed9f35b49462f7e4a3e91ef5
robiul0278 <robiul0278@gmail.com>
2023-04-10 05:53:51 +0600

    4 category card data added

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Featured = () => {
     return (
-        <div>
-            oi
+        <div className='container mx-auto mt-32'>
+            <div className='flex justify-center items-center flex-col'>
+                <h1 className='font-bold'>Featured Jobs</h1>
+                <p className=' text-slate-500 mt-5' >Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div className='grid grid-cols-4 mt-8'>
+
+            </div>
         </div>
     );
 };

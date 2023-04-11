dd4553179c422a9248db4159b61f2eb68d9ac827
robiul0278 <robiul0278@gmail.com>
2023-04-12 02:52:45 +0600

    added complete hero area all section

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Statistics = () => {
-    return (
-        <div>
-            Statistic is here
+  return (
+    <div>
+      <div>
+        <div className="detailsArea grid grid-cols-12">
+          <div className=" col-span-3 bottom-0 left-0">
+            <img className="w-80" src="/public/Image/Vector.png" alt="" />
+          </div>
+          <div className=" col-span-6 flex justify-center items-center">
+            <h1 className="text-center font-bold">Statistic</h1>
+          </div>
+          <div className=" col-span-3 absolute top-0 right-0">
+            <img src="/public/Image/Vector-1.png" alt="" />
+          </div>
         </div>
-    );
+      </div>
+      <div className="container mx-auto">
+
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;

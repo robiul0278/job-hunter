5fe1cde754540b60bd81f1def6a3d613ae5a38ee
robiul0278 <robiul0278@gmail.com>
2023-04-12 21:22:13 +0600

    error page style add

--- src/components/Error/ErrorPage.jsx
+++ src/components/Error/ErrorPage.jsx
@@ -9,17 +9,15 @@ const ErrorPage = () => {
   const { error, status } = useRouteError();
   return (
     <body className="body">
-            <section className="notFound">
+        <section className="notFound">
         <div className="img">
         <img src="https://assets.codepen.io/5647096/backToTheHomepage.png" alt="Back to the Homepage"/>
         <img src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly"/>
         </div>
         <div className="text">
-        <h1 className="font">{status || 404}</h1>
+          <h1 className="font">{status || 404}</h1>
         <h2 className="font">{error?.message}</h2>
-        <h3 className="font">BACK TO HOME?</h3>
-        <Link to="/" className="yes font">YES</Link>
-        <a className="font" href="https://www.youtube.com/@ProgrammingHero">NO</a>
+        <h3 className="font">BACK TO HOME? <Link to="/" className="yes btn px-4 font"> Home </Link></h3>
         </div>
     </section>
     </body>
